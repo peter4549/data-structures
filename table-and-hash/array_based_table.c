70c97670caa69a8f547ee1977f2fc83d5c63bde9
peter4549 <42578653+peter4549@users.noreply.github.com>
2020-09-29 22:49:22 +0900

    Update array_based_table.c

--- table-and-hash/array_based_table.c
+++ table-and-hash/array_based_table.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+typedef struct _employee {
+	int employeeNumber;
+	int age;
+} Employee;
+
+int main(void) {
+	Employee employees[1 << 10];
+	Employee employee;
+	int employeeNumber;
+
+	printf("Employee number and age: ");
+	scanf_s("%d %d", &(employee.employeeNumber), &(employee.age));
+	employees[employee.employeeNumber] = employee;
+
+	printf("Employee number: ");
+	scanf_s("%d", &employeeNumber);
+
+	employee = employees[employeeNumber];
+	printf("Employee number: %d, Age: %d \n", employee.employeeNumber, employee.age);
+
+	return 0;
+}
