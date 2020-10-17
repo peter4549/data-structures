c56cd64a36609ac91ec4df005be7020e63b2afc8
peter4549 <42578653+peter4549@users.noreply.github.com>
2020-10-18 00:30:02 +0900

    Create LinearSearch.c

---
+++ Introduction to Data Structures Using C/LinearSearch.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <time.h>
+
+#define ARRAY_SIZE 8
+#define RANGE 8
+
+void generateRandomNonNegativeIntegers(int integers[], int length, int range) {
+	srand((int) time(NULL));
+
+	for (int i = 0; i < length; ++i)
+		integers[i] = rand() % range;
+}
+
+int linearSearch(int integers[], int length, int target) {
+	for (int i = 0; i < length; ++i) {
+		if (integers[i] == target)
+			return i;
+	}
+
+	return -1;
+}
+
+int main(void) {
+	int integers[ARRAY_SIZE] = { 0, };
+
+	generateRandomNonNegativeIntegers(integers, ARRAY_SIZE, RANGE);
+
+	int index;
+	index = linearSearch(integers, sizeof(integers) / sizeof(int), 3);
+
+	if (index == -1)
+		printf("Search failure. \n");
+	else
+		printf("Target index: %d \n", index);
+
+	index = linearSearch(integers, sizeof(integers) / sizeof(int), 7);
+
+	if (index == -1)
+		printf("Search failure. \n");
+	else
+		printf("Target index: %d \n", index);
+
+	return 0;
+}
