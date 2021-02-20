da5695e8bd90bbd044027024793c2d482b125647
peter4549 <42578653+peter4549@users.noreply.github.com>
2021-02-20 13:16:55 +0900

    Update main.c

--- Introduction to Data Structures Using C/Chapter 06 스택(Stack)/infix_notation_to_postfix_notation/main.c
+++ Introduction to Data Structures Using C/Chapter 06 스택(Stack)/infix_notation_to_postfix_notation/main.c
@@ -2,17 +2,17 @@
 #include "infix_notation_to_postfix_notation.h"
 
 int main() {
-	char infix_notation_0[] = "1+2*3";
-	char infix_notation_1[] = "(1+2)*3";
-	char infix_notation_2[] = "((1-2)+3)*(5-2)";
+	char infix_notation0[] = "1+2*3";
+	char infix_notation1[] = "(1+2)*3";
+	char infix_notation2[] = "((1-2)+3)*(5-2)";
 	
-	ConvertToPostfixNotation(infix_notation_0);
-	ConvertToPostfixNotation(infix_notation_1);
-	ConvertToPostfixNotation(infix_notation_2);
+	ConvertToPostfixNotation(infix_notation0);
+	ConvertToPostfixNotation(infix_notation1);
+	ConvertToPostfixNotation(infix_notation2);
 
-	printf("%s \n", infix_notation_0);
-	printf("%s \n", infix_notation_1);
-	printf("%s \n", infix_notation_2);
+	printf("%s \n", infix_notation0);
+	printf("%s \n", infix_notation1);
+	printf("%s \n", infix_notation2);
 	
 	return 0;
 }
